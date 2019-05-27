807d386cb708240a77be4c91046c0df0f736194d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-05-27 12:04:54 -0500

    Actualiza archivos de configuración

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -6,16 +6,16 @@
 #
 # Read the Rails 5.0 release notes for more info on each option.
 
-# Enable per-form CSRF tokens. Previous versions had false.
-Rails.application.config.action_controller.per_form_csrf_tokens = false
+# Enable per-form CSRF tokens. Previous versions had true.
+Rails.application.config.action_controller.per_form_csrf_tokens = true
 
-# Enable origin-checking CSRF mitigation. Previous versions had false.
-Rails.application.config.action_controller.forgery_protection_origin_check = false
+# Enable origin-checking CSRF mitigation. Previous versions had true.
+Rails.application.config.action_controller.forgery_protection_origin_check = true
 
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
-# Previous versions had false.
-ActiveSupport.to_time_preserves_timezone = false
+# Previous versions had true.
+ActiveSupport.to_time_preserves_timezone = true
 
-# Require `belongs_to` associations by default. Previous versions had false.
-Rails.application.config.active_record.belongs_to_required_by_default = false
+# Require `belongs_to` associations by default. Previous versions had true.
+Rails.application.config.active_record.belongs_to_required_by_default = true
 
