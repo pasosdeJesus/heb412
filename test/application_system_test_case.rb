7458e5cce3f2d5381dd6265dd4d0bd205dd58d51
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-07-26 15:57:02 -0500

    Simplifica

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -16,16 +16,6 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 #    debug: true 
   }
 
-  Capybara.register_driver :headless_chrome do |app|
-      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-            chromeOptions: { args: %w(headless disable-gpu) }
-      )
-
-      Capybara::Selenium::Driver.new app,
-        browser: :chrome,
-        desired_capabilities: capabilities
-  end
-
   def setup
 #    load "#{Rails.root}/db/seeds.rb"
   end
