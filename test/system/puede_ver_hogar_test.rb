d99c00804a0ccbe43b8156e290483147b155c4e9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-06-20 14:51:42 -0500

    Actualiza

--- test/system/puede_ver_hogar_test.rb
+++ test/system/puede_ver_hogar_test.rb
@@ -5,7 +5,6 @@ require "application_system_test_case"
 class PuedeVerHogarTest < ApplicationSystemTestCase
 
   test "hogar con contenido" do 
-    skip
     visit Rails.configuration.relative_url_root
     take_screenshot
     assert page.has_content?("Heb412")
