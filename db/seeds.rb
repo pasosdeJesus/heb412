266fca77cbbe60c33af1676349500228d2877e0c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-02 18:54:06 -0500

    Actualiza

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ end
 
 # Usuario para primer ingreso heb412, heb412
 conexion.execute("INSERT INTO public.usuario 
-	(nusuario, email, encrypted_password, password, 
+	(id, nusuario, email, encrypted_password, password, 
   fechacreacion, created_at, updated_at, rol) 
-	VALUES ('heb412', 'heb412@localhost', 
+	VALUES (1, 'heb412', 'heb412@localhost', 
 	'$2a$10$FdubMrU.LZmkqMCFIaVXWORiOIQAM8/AqRrlC3SlkKsiZNCV/mmpC',  
 	'', '2014-08-14', '2014-08-14', '2014-08-14', 1);")
 
