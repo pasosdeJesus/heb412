f861e815ec19230966674960d9fbeaf1ec011935
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-10-25 16:47:55 -0500

    Actualiza

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 class Ability  < Heb412Gen::Ability
 
   def acciones_plantillas
@@ -6,48 +5,7 @@ class Ability  < Heb412Gen::Ability
 
   # Autorizacion con CanCanCan
   def initialize(usuario = nil)
-    # Sin autenticación puede consultarse información geográfica 
-
-    can :read, [Sip::Pais, Sip::Departamento, Sip::Municipio, Sip::Clase]
-    if !usuario || usuario.fechadeshabilitacion
-      return
-    end
-    can :descarga_anexo, Sip::Anexo
-    can :contar, Sip::Ubicacion
-    can :buscar, Sip::Ubicacion
-    can :lista, Sip::Ubicacion
-    can :nuevo, Sip::Ubicacion
-    if usuario && usuario.rol then
-      can :read, Heb412Gen::Doc
-      can :read, Heb412Gen::Plantilladoc
-      can :read, Heb412Gen::Plantillahcm
-      can :read, Heb412Gen::Plantillahcr
-      case usuario.rol 
-      when Ability::ROLANALI
-        can [:read, :new, :create, :update], Sip::Orgsocial
-        can :manage, Sip::Persona
-        can [:read, :new, :update, :create, :destroy], Sip::Ubicacion
-      when Ability::ROLADMIN
-        can :manage, Heb412Gen::Doc
-        can :manage, Heb412Gen::Plantilladoc
-        can :manage, Heb412Gen::Plantillahcm
-        can :manage, Heb412Gen::Plantillahcr
-        
-        can :manage, Mr519Gen::Formulario
-
-        can :manage, Sip::Orgsocial
-        can :manage, Sip::Persona
-        can :manage, Sip::Ubicacion
-        can :manage, Sip::Ubicacionpre
-
-        can :manage, ::Usuario
-        can :manage, :tablasbasicas
-        tablasbasicas.each do |t|
-          c = Ability.tb_clase(t)
-          can :manage, c
-        end
-      end
-    end
+    initialize_heb412_gen(usuario)
   end
 
 end
