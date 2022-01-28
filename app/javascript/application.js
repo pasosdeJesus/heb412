6e075a62906772b95f6efe43dd93d30f382ca512
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-28 09:44:15 -0500

    Renombrando recursos_globales por recursos_sprockets y afinando arranque javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -19,3 +19,40 @@ import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
 
 import 'gridstack'
 
+let esperarRecursosSprocketsYDocumento = function (resolver) {
+  if (typeof window.puntomontaje == 'undefined') {
+    setTimeout(esperarRecursosSprocketsYDocumento, 100, resolver)
+    return false
+  }
+  if (document.readyState !== 'complete') {
+    setTimeout(esperarRecursosSprocketsYDocumento, 100, resolver)
+    return false
+  }
+  resolver("Recursos manejados con sprockets cargados y documento presentado en navegador")
+    return true
+  }
+
+let promesaRecursosSprocketsYDocumento = new Promise((resolver, rechazar) => {
+  esperarRecursosSprocketsYDocumento(resolver)
+})
+
+promesaRecursosSprocketsYDocumento.then((mensaje) => {
+  console.log(mensaje)
+  var root = window;
+  sip_prepara_eventos_comunes(root);
+  mr519_gen_prepara_eventos_comunes(root);
+  heb412_gen_prepara_eventos_comunes(root);
+})
+
+
+document.addEventListener('turbo:load', (e) => {
+ /* Lo que debe ejecutarse cada vez que turbo cargue una página,
+ * tener cuidado porque puede dispararse el evento turbo varias
+ * veces consecutivas al cargarse  la misma página.
+ */
+  
+  console.log('Escuchador turbo:load')
+
+  sip_ejecutarAlCargarPagina(window)
+})
+
