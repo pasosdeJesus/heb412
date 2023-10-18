d24da15f49c670b5848489150816cef8c275470d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-10-18 11:51:41 -0500

    Actualiza

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 
+require "simplecov_json_formatter"
 require 'simplecov'
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 SimpleCov.start 'rails'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
