ffe1db79475ff6f6fc65c29405618c93750db8e6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-02-09 18:51:14 -0500

    sin heroku

--- config/initializers/msip.rb
+++ config/initializers/msip.rb
@@ -5,10 +5,6 @@ Msip.setup do |config|
                                  "#{Rails.root}/archivos/anexos")
   config.ruta_volcados = ENV.fetch('MSIP_RUTA_VOLCADOS',
                                    "#{Rails.root}/archivos/bd")
-  # En heroku los anexos son super-temporales
-  if !ENV["HEROKU_POSTGRESQL_GREEN_URL"].nil?
-    config.ruta_anexos = "#{Rails.root}/tmp/"
-  end
   config.titulo = "Heb412 - #{Heb412Gen::VERSION}"
   config.descripcion = "Plantilla para una aplicación web con autenticación, nube y geografía de Colombia"
   config.codigofuente = "https://gitlab.com/pasosdeJesus/heb412"
